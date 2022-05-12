60aee455a1db9622fb338326c6b62963e170df67
jsonwan <1334793794@qq.com>
2022-05-12 14:56:57 +0800

    perf: 第三方文件源文件分发文件准备过程失败时，返回给调用方失败原因 #785
    ServiceFileTaskLogDTO改造为建造者模式

--- src/backend/job-logsvr/api-job-logsvr/src/main/java/com/tencent/bk/job/logsvr/model/service/ServiceFileTaskLogDTO.java
+++ src/backend/job-logsvr/api-job-logsvr/src/main/java/com/tencent/bk/job/logsvr/model/service/ServiceFileTaskLogDTO.java
@@ -26,6 +26,7 @@ package com.tencent.bk.job.logsvr.model.service;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -36,6 +37,8 @@ import org.apache.commons.lang3.StringUtils;
  */
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ServiceFileTaskLogDTO {
     /**
